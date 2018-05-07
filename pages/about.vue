3e820b43ad9f8482c16f3aa6d1596829331e4ba7
Renan Paiva Bonette <renan.bonette@gmail.com>
2018-05-07 15:00:50 -0300

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,11 @@
+<template>
+  <section class="section is-medium">
+    <div class="container has-text-centered">
+      <h1 class="title">About Me</h1>
+      <p>
+        My name is Renan Bonette , I’m a software engineer and innovation consultor based in Sao Paulo, Brazil.
+        I build modern applications for the web!
+      </p>
+    </div>
+  </section>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <section class="section is-large">
+    <div class="container has-text-centered">
+      <h1 class="title">Hi, I'm Renan Bonette</h1>
+      <h2 class="subtitle">a Software Enginner based in Sao Paulo, Brazil.</h2>
+    </div>
+  </section>
+</template>
